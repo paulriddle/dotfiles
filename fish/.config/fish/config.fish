15d75cd3811d516ee7b6ec6fb66d4d92badd4e31
Paul Riddle <paulriddle@openmailbox.org>
2015-07-19 23:49:53 +0500

    Workaround for nvm due to bass bug or feature

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,6 @@ eval sh $HOME/.config/base16-shell/base16-default.dark.sh
 
 # Initialize rbenv
 source (rbenv init - | psub)
+
+# Temporary workaround for nvm
+set -x NVM_NODEJS_ORG_MIRROR https://nodejs.org/dist
