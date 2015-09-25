a17c86d8fa8605361b548c637e584677f6c01cf0
Paul Riddle <paulriddle@openmailbox.org>
2015-09-25 16:53:37 +0500

    add ssh key to keychain

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,6 @@ eval sh $HOME/.config/base16-shell/base16-atelierheath.light.sh
 
 # Initialize rbenv
 source (rbenv init - | psub)
+
+# Run keychain
+eval (keychain --eval -Q --quiet galkin-20150916)
