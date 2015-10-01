c27917e641009c7c888b62079051baf7cad195fc
Paul Riddle <paulriddle@openmailbox.org>
2015-10-01 11:53:57 +0500

    replace rbenv with rvm

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,6 @@
 # Base16 color scheme
 eval sh $HOME/.config/base16-shell/base16-atelierheath.light.sh
 
-# Initialize rbenv
-source (rbenv init - | psub)
-
 # Run keychain
 eval (keychain --eval -Q --quiet galkin-20150916)
+rvm default
