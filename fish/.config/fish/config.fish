1d5a266a34cf462fa18dbe9fbc47a13d5405cbb0
Paul Riddle <paulriddle@openmailbox.org>
2015-10-20 14:42:14 +0500

    add env vars for old ruby optimization

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,8 @@
 eval sh $HOME/.config/base16-shell/base16-chalk.dark.sh
 eval (keychain --eval -Q --quiet galkin-20150916)
-rvm default
+
+status --is-interactive; and . (rbenv init -|psub)
+
+# set -x RUBY_GC_MALLOC_LIMIT 1000000000
+# set -x RUBY_FREE_MIN 500000
+# set -x RUBY_HEAP_MIN_SLOTS 40000
