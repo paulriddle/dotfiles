9d6fcb8a5995de12d4baf18f933094a29d9b55fe
Paul Riddle <paulriddle@openmailbox.org>
2015-10-26 06:04:08 +0500

    initialize rbenv

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,5 @@
 eval sh $HOME/.config/base16-shell/base16-chalk.dark.sh
+status --is-interactive; and . (rbenv init - | psub)
 
 # set -x RUBY_GC_MALLOC_LIMIT 1000000000
 # set -x RUBY_FREE_MIN 500000
