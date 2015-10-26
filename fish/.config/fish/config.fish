2d9641c9a3e028d113548116b9b51746422eb2fd
Paul Riddle <paulriddle@openmailbox.org>
2015-10-26 05:56:54 +0500

    cleanup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,4 @@
 eval sh $HOME/.config/base16-shell/base16-chalk.dark.sh
-eval (keychain --eval -Q --quiet galkin-20150916)
-
-status --is-interactive; and . (rbenv init -|psub)
 
 # set -x RUBY_GC_MALLOC_LIMIT 1000000000
 # set -x RUBY_FREE_MIN 500000
