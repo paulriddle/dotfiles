b5f53135dfb933a7f92e6b86e30091020b0dc479
Pavel Galkin <paulriddle@openmailbox.org>
2015-11-16 11:41:09 +0500

    put ruby env vars on single line

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,4 @@ eval sh $HOME/.config/base16-shell/base16-chalk.dark.sh
 status --is-interactive; and . (rbenv init - | psub)
 start_agent
 
-# set -x RUBY_GC_MALLOC_LIMIT 1000000000
-# set -x RUBY_FREE_MIN 500000
-# set -x RUBY_HEAP_MIN_SLOTS 40000
+# set -x RUBY_GC_MALLOC_LIMIT 1000000000; set -x RUBY_FREE_MIN 500000; set -x RUBY_HEAP_MIN_SLOTS 40000
