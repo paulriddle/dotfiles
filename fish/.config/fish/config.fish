b5158f453fd0df06befe2d1db75624501b7f7a9c
Pavel Galkin <paulriddle@openmailbox.org>
2016-10-01 18:41:19 +0500

    Solarized light again

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 if status --is-interactive
-  eval sh $HOME/.config/base16-shell/scripts/base16-materia.sh
+  eval sh $HOME/.config/base16-shell/scripts/base16-solarized-light.sh
 end
 status --is-interactive; and . (rbenv init - | psub)
 start_agent
