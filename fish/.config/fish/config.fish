672bbf973617251f4fc30ff5a6fd96c9a0b1c919
Pavel Galkin <paulriddle@openmailbox.org>
2016-10-19 21:27:28 +0500

    Use ocean colorscheme. Summerfruit is too bright.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 if status --is-interactive
-  eval sh $HOME/.config/base16-shell/scripts/base16-summerfruit-light.sh
+  eval sh $HOME/.config/base16-shell/scripts/base16-ocean.sh
 end
 status --is-interactive; and . (rbenv init - | psub)
 start_agent
