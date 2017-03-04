e6b3d5cbfc8c47f20087d61e338fd4fda953ad9e
Pavel Galkin <paulriddle@openmailbox.org>
2017-03-04 10:29:43 +0500

    Use gruvbox light medium colorscheme

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 if status --is-interactive
-  eval sh $HOME/.config/base16-shell/scripts/base16-railscasts.sh
+  eval sh $HOME/.config/base16-shell/scripts/base16-gruvbox-light-medium.sh
 end
 status --is-interactive; and . (rbenv init - | psub)
 start_agent
