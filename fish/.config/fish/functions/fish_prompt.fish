c0cbab26cc70b480ff04216de20211c9f5f77669
Pavel Galkin <paulriddle@openmailbox.org>
2017-10-03 15:54:50 +0500

    return the old fish prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,18 +1,16 @@
 function fish_prompt --description 'Write out the prompt'
-	set -l color_cwd
-    set -l suffix
-    switch $USER
-        case root toor
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
-            set suffix '#'
-        case '*'
-            set color_cwd $fish_color_cwd
-            set suffix '>'
-    end
+	# Just calculate these once, to save a few cycles when displaying the prompt
+   if not set -q __fish_prompt_hostname
+       set -g __fish_prompt_hostname (hostname -s)
+   end
 
-    echo -n -s "$USER" @ (prompt_hostname) ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal) "$suffix "
+   if not set -q __fish_prompt_normal
+       set -g __fish_prompt_normal (set_color normal)
+   end
+
+   if not set -q __fish_prompt_cwd
+       set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+   end
+
+   echo -n -s "$USER" @ "$__fish_prompt_hostname" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" '> '
 end
