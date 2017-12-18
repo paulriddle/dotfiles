d045003fd5a7087cefcb9fb6becc12294b11a3fd
Pavel Galkin <paulriddle@openmailbox.org>
2017-12-18 10:25:17 +0500

    fish: initialize rbenv

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,5 @@
 if status --is-interactive
+  source (rbenv init -|psub)
   eval sh $HOME/.config/base16-shell/scripts/base16-one-light.sh
   # eval sh $HOME/.config/base16-shell/scripts/base16-gruvbox-dark-hard.sh
 end
