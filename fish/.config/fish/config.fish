b367ca9a27a3a6a4310ed1773e7daa2acf93067d
Pavel Galkin <paulriddle@openmailbox.org>
2017-12-22 23:48:35 +0500

    fish: fix typo in colorscheme name

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ if status --is-interactive
   if [ "$LIGHT_COLORSCHEME" -eq 1 ]
     eval sh $HOME/.config/base16-shell/scripts/base16-gruvbox-light-medium.sh
   else
-    eval sh $HOME/.config/base16-shell/scripts/base16-irrblack.sh
+    eval sh $HOME/.config/base16-shell/scripts/base16-irblack.sh
   end
 end
 
