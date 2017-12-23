ed42ee970bbf247aa0c33a05347f217a87a18b2f
Pavel Galkin <paulriddle@openmailbox.org>
2017-12-23 13:45:55 +0500

    nvim: fix FZF_DEFAULT_COMMAND by removing --glob

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,7 +11,7 @@ start_agent
 set -x GOPATH ~/gocode
 set -x FISH_CONFIG ~/.config/fish/config.fish
 set -x I3 ~/.config/i3/config
-set -x FZF_DEFAULT_COMMAND 'rg --files --ignore-case --glob'
+set -x FZF_DEFAULT_COMMAND 'rg --files --ignore-case'
 set -x FZF_CTRL_T_COMMAND 'rg --files --ignore-case'
 set -x FZF_DEFAULT_OPTS '--height 30%'
 set -x MYVIMRC ~/.config/nvim/init.vim
