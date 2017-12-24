6560dfb6a7d9100f6f4559becf4726ec42ee9344
Pavel Galkin <paulriddle@openmailbox.org>
2017-12-24 23:49:17 +0500

    nvim: source rbenv on startup
    Finally installed Ruby.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,6 @@
 if status --is-interactive
+    source (rbenv init -|psub)
+
   if [ "$LIGHT_COLORSCHEME" -eq 1 ]
     eval sh $HOME/.config/base16-shell/scripts/base16-gruvbox-light-medium.sh
   else
