c39157c8e14bec46edc75e396eafc7a33b1c94a2
Pavel Galkin <paulriddle@openmailbox.org>
2017-12-29 06:53:00 +0500

    fish: adjust indentation

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 if status --is-interactive
-    source (rbenv init -|psub)
+  source (rbenv init -|psub)
 
   if [ "$LIGHT_COLORSCHEME" -eq 1 ]
     eval sh $HOME/.config/base16-shell/scripts/base16-gruvbox-light-medium.sh
