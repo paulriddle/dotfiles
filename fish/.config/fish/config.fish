155f98b64461168bbc35697755e8215d8f7f57e5
Pavel Galkin <paulriddle@openmailbox.org>
2017-12-30 20:19:39 +0500

    fish: solarized-light colorscheme

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ if status --is-interactive
   source (rbenv init -|psub)
 
   if [ "$LIGHT_COLORSCHEME" -eq 1 ]
-    eval sh $HOME/.config/base16-shell/scripts/base16-gruvbox-light-medium.sh
+    eval sh $HOME/.config/base16-shell/scripts/base16-solarized-light.sh
   else
     eval sh $HOME/.config/base16-shell/scripts/base16-railscasts.sh
   end
