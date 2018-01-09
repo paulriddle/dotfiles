1013183035509a2db76b076ba6d93099c1bd826d
Pavel Galkin <paulriddle@openmailbox.org>
2018-01-09 11:35:45 +0500

    fish: use default GOPATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,7 +9,6 @@ if status --is-interactive
 end
 
 start_agent
-set -x GOPATH ~/gocode
 set -x FISH_CONFIG ~/.config/fish/config.fish
 set -x I3 ~/.config/i3/config
 set -x FZF_DEFAULT_COMMAND 'rg --files --ignore-case'
