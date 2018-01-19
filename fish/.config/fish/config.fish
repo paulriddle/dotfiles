0c584b51f5ee22a646e18bfc161047c9aab89ead
Pavel Galkin <paulriddle@openmailbox.org>
2018-01-19 13:24:23 +0500

    fish: change colorschemes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,9 +2,9 @@ if status --is-interactive
   source (rbenv init -|psub)
 
   if [ "$LIGHT_COLORSCHEME" -eq 1 ]
-    eval sh $HOME/.config/base16-shell/scripts/base16-github.sh
+    eval sh $HOME/.config/base16-shell/scripts/base16-harmonic-light.sh
   else
-    eval sh $HOME/.config/base16-shell/scripts/base16-railscasts.sh
+    eval sh $HOME/.config/base16-shell/scripts/base16-harmonic-dark.sh
   end
 end
 
