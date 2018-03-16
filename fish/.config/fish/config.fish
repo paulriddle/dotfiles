79a6bb450292b00453db284cd1c01c9e4c11b53b
Pavel Galkin <paulriddle@openmailbox.org>
2018-03-16 09:49:51 +0500

    Change colorscheme and font

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ if status --is-interactive
   source (rbenv init -|psub)
 
   if [ "$LIGHT_COLORSCHEME" -eq 1 ]
-    eval sh $HOME/.config/base16-shell/scripts/base16-solarized-light.sh
+    eval sh $HOME/.config/base16-shell/scripts/base16-github.sh
   else
     eval sh $HOME/.config/base16-shell/scripts/base16-brushtrees-dark.sh
   end
