f75c9ba05fc187a8a1769d3a154307fba9320a52
Pavel Galkin <paulriddle@openmailbox.org>
2018-05-19 21:12:31 +0500

    Specify address for nvim/nvr to listen to

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -16,3 +16,4 @@ set -x FZF_DEFAULT_COMMAND 'rg --files --ignore-case'
 set -x FZF_CTRL_T_COMMAND 'rg --files --ignore-case'
 set -x FZF_DEFAULT_OPTS '--height 30%'
 set -x MYVIMRC ~/.config/nvim/init.vim
+set -x NVIM_LISTEN_ADDRESS /tmp/nvimsocket
