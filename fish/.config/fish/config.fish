f33c97d21bfc37bfd170aac6121ed2fd1bfd456c
Pavel Galkin <paulriddle@openmailbox.org>
2018-06-03 21:59:37 +0500

    Set default keyboard config for Sway
    Now it's done this way instead of ~/.xinitrc.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,3 +12,5 @@ set -x FZF_CTRL_T_COMMAND 'rg --files --ignore-case'
 set -x FZF_DEFAULT_OPTS '--height 30%'
 set -x MYVIMRC ~/.config/nvim/init.vim
 set -x NVIM_LISTEN_ADDRESS /tmp/nvimsocket
+set -x XKB_DEFAULT_LAYOUT us,ru
+set -x XKB_DEFAULT_OPTIONS grp:alt_shift_toggle,caps:ctrl_modifier
