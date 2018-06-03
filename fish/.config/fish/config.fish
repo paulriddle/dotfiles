2c58173576f6fc8b560ad5ae80980cea64967e41
Pavel Galkin <paulriddle@openmailbox.org>
2018-06-04 00:06:40 +0500

    Remove rarely used env variable

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,7 +10,6 @@ end
 
 start_agent
 set -x FISH_CONFIG ~/.config/fish/config.fish
-set -x I3 ~/.config/i3/config
 set -x FZF_DEFAULT_COMMAND 'rg --files --ignore-case'
 set -x FZF_CTRL_T_COMMAND 'rg --files --ignore-case'
 set -x FZF_DEFAULT_OPTS '--height 30%'
