f277c32eb3bd131300f4c5eb8b1a37745f9e01cb
Pavel Galkin <paulriddle@openmailbox.org>
2018-07-25 16:04:21 +0500

    Colors

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,5 @@
 if status --is-interactive
   source (rbenv init -|psub)
-
-  if [ "$LIGHT_COLORSCHEME" -eq 1 ]
-    eval sh $HOME/.config/base16-shell/scripts/base16-github.sh
-  else
-    eval sh $HOME/.config/base16-shell/scripts/base16-classic-dark.sh
-  end
 end
 
 start_agent
