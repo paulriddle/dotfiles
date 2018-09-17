432ea8212ca80693d87e735c902e563135af0e28
Pavel Galkin <paulriddle@openmailbox.org>
2018-09-17 18:00:31 +0500

    fzf: switch from rg to fd

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,8 +7,8 @@ end
 
 start_agent
 set -x FISH_CONFIG ~/.config/fish/config.fish
-set -x FZF_DEFAULT_COMMAND 'rg --files --ignore-case'
-set -x FZF_CTRL_T_COMMAND 'rg --files --ignore-case'
+set -x FZF_DEFAULT_COMMAND 'fd --type f'
+set -x FZF_CTRL_T_COMMAND 'fd --type f'
 set -x FZF_DEFAULT_OPTS '--height 30%'
 set -x MYVIMRC ~/.config/nvim/init.vim
 set -x NVIM_LISTEN_ADDRESS /tmp/nvimsocket
