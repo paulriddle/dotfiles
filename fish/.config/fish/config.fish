556a98d8cdafcc5f58212a10fcfb175a557afbb5
Pavel Galkin <paulriddle@openmailbox.org>
2019-06-02 10:17:29 +0500

    Get rid of duplicated entries in $PATH
    For some reason $PATH contained duplicated entries specified by
    $fish_user_paths. Furthermore, appending to $fish_user_paths complained
    about being shadowed by a global variable fish_user_paths. As a result
    adding new entry to $PATH was messy, I didn't understand the process,
    didn't understand what was happening and decided to get rid of universal
    variable $fish_user_paths completely.
    
    Now $PATH is expanded explicitly in config.fish. Duplication still
    happened though. The most likely culprit is alacritty. To fix
    duplication I had to change my shell to bash via chsh. It worked fine
    because Sway uses alacritty, and alacritty uses fish. So even though my
    login shell is bash, in practice I'm still using fish. It might lead to
    some problems later when programs rely on my default shell. $SHELL is
    now bash.
    
    Because of this shell change I also had to specify bindings in Sway
    config. I suspect input device name is not portable.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,15 +1,18 @@
-if status --is-interactive
-  source (rbenv init -|psub)
-end
-
 start_agent
 
-set -x FZF_DEFAULT_COMMAND 'rg --files --ignore-case'
-set -x FZF_CTRL_T_COMMAND 'rg --files --ignore-case'
-set -x FZF_DEFAULT_OPTS '--height 30%'
-set -x NVIM_LISTEN_ADDRESS /tmp/nvimsocket
-set -x XKB_DEFAULT_LAYOUT us,ru
-set -x XKB_DEFAULT_OPTIONS grp:alt_shift_toggle,caps:ctrl_modifier
-set -x EDITOR nvim
-set -x NNN_USE_EDITOR 1
-set -x DOCKER_TLD localhost
+if status --is-interactive
+  set PATH $HOME/.local/bin $HOME/.rbenv/bin $HOME/.fzf/bin $HOME/.cargo/bin $PATH
+
+  source (rbenv init - | psub)
+
+  set -x FZF_DEFAULT_COMMAND 'rg --files --ignore-case'
+  set -x FZF_CTRL_T_COMMAND 'rg --files --ignore-case'
+  set -x FZF_DEFAULT_OPTS '--height 30%'
+
+  set -x NVIM_LISTEN_ADDRESS /tmp/nvimsocket
+
+  set -x EDITOR nvim
+  set -x NNN_USE_EDITOR 1
+
+  set -x DOCKER_TLD localhost
+end
