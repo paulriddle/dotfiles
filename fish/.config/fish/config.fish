bc2179448a5e55734be316c60510e404667ac818
Pavel Galkin <paulriddle@openmailbox.org>
2019-08-30 14:50:44 +0500

    Remove commented out commands fron config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,8 +9,6 @@ set -x FZF_CTRL_T_COMMAND 'fd --type f'
 set -x FZF_DEFAULT_OPTS '--height 30%'
 set -x MYVIMRC ~/.config/nvim/init.vim
 set -x NVIM_LISTEN_ADDRESS /tmp/nvimsocket
-# set -x XKB_DEFAULT_LAYOUT us,ru
-# set -x XKB_DEFAULT_OPTIONS grp:alt_shift_toggle,caps:ctrl_modifier
 set -x EDITOR nvim
 set -x DOCKER_TLD localhost
 set -x _JAVA_AWT_WM_NONREPARENTING 1
