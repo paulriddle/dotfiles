dca4932660e524823393708cbd1c583d5c065374
Pavel Galkin <fixnum42@gmail.com>
2019-08-30 21:37:54 +0500

    Change default fzf command
    fd seems more appropriate.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,8 @@
 start_agent
 
 if status --is-interactive
-  set -x FZF_DEFAULT_COMMAND 'rg --files --ignore-case'
-  set -x FZF_CTRL_T_COMMAND 'rg --files --ignore-case'
+  set -x FZF_DEFAULT_COMMAND 'fd --type f'
+  set -x FZF_CTRL_T_COMMAND 'fd --type f'
   set -x FZF_DEFAULT_OPTS '--height 30%'
 
   set -x NVIM_LISTEN_ADDRESS /tmp/nvimsocket
