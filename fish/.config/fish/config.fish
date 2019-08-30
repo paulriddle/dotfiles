439bafdbbd333d5e82f47d0ab9b822c5504a2cc2
Pavel Galkin <fixnum42@gmail.com>
2019-08-30 21:38:33 +0500

    Remove --is-interactive check
    I run fish mostly in interactive mode.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,9 @@
 start_agent
 
-if status --is-interactive
-  set -x FZF_DEFAULT_COMMAND 'fd --type f'
-  set -x FZF_CTRL_T_COMMAND 'fd --type f'
-  set -x FZF_DEFAULT_OPTS '--height 30%'
-
-  set -x NVIM_LISTEN_ADDRESS /tmp/nvimsocket
-
-  set -x EDITOR nvim
-  set -x NNN_USE_EDITOR 1
-
-  set -x DOCKER_TLD localhost
-end
+set -x FZF_DEFAULT_COMMAND 'fd --type f'
+set -x FZF_CTRL_T_COMMAND 'fd --type f'
+set -x FZF_DEFAULT_OPTS '--height 30%'
+set -x NVIM_LISTEN_ADDRESS /tmp/nvimsocket
+set -x EDITOR nvim
+set -x NNN_USE_EDITOR 1
+set -x DOCKER_TLD localhost
