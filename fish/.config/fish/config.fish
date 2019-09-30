676777ef20d70c0cd98364d90f80f5a765b7225a
Pavel Galkin <paulriddle@openmailbox.org>
2019-09-30 13:54:14 +0500

    Vim as MANPAGE

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,8 +7,11 @@ set -x FISH_CONFIG ~/.config/fish/config.fish
 set -x FZF_DEFAULT_COMMAND 'fd --type f'
 set -x FZF_CTRL_T_COMMAND 'fd --type f'
 set -x FZF_DEFAULT_OPTS '--height 30%'
+
 set -x MYVIMRC ~/.config/nvim/init.vim
 set -x NVIM_LISTEN_ADDRESS /tmp/nvimsocket
+set -x MANPAGER "nvim -c 'set ft=man' -"
 set -x EDITOR nvim
+
 set -x DOCKER_TLD localhost
 set -x _JAVA_AWT_WM_NONREPARENTING 1
