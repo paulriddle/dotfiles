c38ded522cc759c32f37639fd24dea0b8f71485d
Pavel Galkin <fixnum42@gmail.com>
2019-10-10 22:19:44 +0500

    Vim as MANPAGER

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,8 +3,11 @@ start_agent
 set -x FZF_DEFAULT_COMMAND 'fd --type f'
 set -x FZF_CTRL_T_COMMAND 'fd --type f'
 set -x FZF_DEFAULT_OPTS '--height 30%'
+
 set -x NVIM_LISTEN_ADDRESS /tmp/nvimsocket
 set -x EDITOR nvim
+set -x MANPAGER "nvim -c 'set ft=man' -"
+
 set -x NNN_USE_EDITOR 1
 set -x DOCKER_TLD localhost
 set -x _JAVA_AWT_WM_NONREPARENTING 1
