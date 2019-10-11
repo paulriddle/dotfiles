1500bef6211b969d1b22699532bf514c71f0f17c
Pavel Galkin <paulriddle@openmailbox.org>
2019-10-11 11:29:03 +0500

    Don't ignore fish functions

---
+++ fish/.config/fish/functions/gcc.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function gcc --description 'alias gcc gcc -q'
+	command gcc -q $argv;
+end
---
+++ fish/.config/fish/functions/vinfo.fish
@@ -0,0 +1,3 @@
+function vinfo
+  nvim -R -M -c "Info $argv[1] $argv[2]" +only
+end
