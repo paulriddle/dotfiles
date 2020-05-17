88d1072ac70a01dcb58eaeacf48692a2e4829859
Pavel Galkin <fixnum42@gmail.com>
2020-05-17 23:10:53 +0500

    Inline and combine sed commands
    When it is a single line, it's easier to copy pase and debug.

--- fish/.config/fish/functions/certspotter.fish
+++ fish/.config/fish/functions/certspotter.fish
@@ -1,8 +1,3 @@
 function certspotter --description 'Finds subdomains using certspotter API'
-  http "https://api.certspotter.com/v1/issuances?domain=$argv[1]&expand=dns_names" |
-    jq '.[].dns_names[]' |
-    sed 's/\"//g' |
-    sed 's/\*\.//g' |
-    sort -u |
-    rg $argv[1]
+  http "https://api.certspotter.com/v1/issuances?domain=$argv[1]&expand=dns_names" | jq '.[].dns_names[]' | sed 's/"//g; s/*\.//g' | sort -u | rg $argv[1]
 end
