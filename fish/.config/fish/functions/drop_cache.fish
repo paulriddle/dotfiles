26169398b023c3ab4d292e977c757a59e8091407
Paul Riddle <fixnum42@gmail.com>
2020-07-15 10:34:43 +0500

    Add function to drop WSL2 caches

---
+++ fish/.config/fish/functions/drop_cache.fish
@@ -0,0 +1,6 @@
+# This is specific to WSL 2. If the WSL 2 VM goes rogue and decides not to free
+# up memory, this command will free your memory after about 20-30 seconds.
+# Details: https://github.com/microsoft/WSL/issues/4166#issuecomment-628493643
+function drop_cache
+  sudo sh -c "echo 3 > '/proc/sys/vm/drop_caches'"; sudo swapoff -a; sudo swapon -a; echo 'RAM cache and swap cleared'
+end
