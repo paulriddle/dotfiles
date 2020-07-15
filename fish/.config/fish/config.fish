2e67d54caa4ea7c2495a7e0e138d4080626058f7
Paul Riddle <fixnum42@gmail.com>
2020-07-15 10:35:28 +0500

    Use default manpager instead of vim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,7 +5,6 @@ set -x FZF_CTRL_T_COMMAND 'fd --type f'
 set -x FZF_DEFAULT_OPTS '--height 30%'
 
 set -x EDITOR nvim
-set -x MANPAGER "nvim -c 'set ft=man' -"
 
 set -x DOCKER_TLD localhost
 set -x FONTCONFIG_PATH /etc/fonts
